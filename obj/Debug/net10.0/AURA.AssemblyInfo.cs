34f4fadb9d732f3ff9d7d58c06b2d297805f752d
Amohelang Mohlomi <mohlomiamohelang3@gmail.com>
2026-01-08 14:34:13 +0200

    moved the style to the app.css file

--- obj/Debug/net10.0/AURA.AssemblyInfo.cs
+++ obj/Debug/net10.0/AURA.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AURA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02e4e5e291c913fa5e2cdf1cdd49df3ebc357fb3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a3c4688e7a87a11fe092f5095fbcf53f9435df3")]
 [assembly: System.Reflection.AssemblyProductAttribute("AURA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AURA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
